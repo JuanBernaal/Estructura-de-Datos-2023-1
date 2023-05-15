65b8088451b7826771c267598d14445410cb7ae4
Juan David <123709870+JuanBernaal@users.noreply.github.com>
2023-05-14 23:17:28 -0500

    Update word.cpp

--- tarea6/word.cpp
+++ tarea6/word.cpp
@@ -1,31 +1,58 @@
 #include <iostream>
-#include <string>
-#include <list>
+#include <map>
+#include <vector>
 
 using namespace std;
+typedef map<char, int> Map;
 
-int main(){
-    list<string> dict, puzzle;
-    string input, x;
-    while(cin >> input && input != "#"){
-        dict.push_back(input);
-    }
-    cin.ignore();
-    while(getline(cin, input) && input != "#"){
-        puzzle.push_back(input);
-    }
-    for(list<string>::iterator it = puzzle.begin(); it != puzzle.end(); it++){
-    string x = *it;
-        for(int j = 0; j < x.size(); j += 2){
-            cout << "Puzzle: " << x[j] << endl;
-            for(list<string>::iterator itr = dict.begin(); itr != dict.end(); itr++){
-                string ref = *itr;
-                for(int i = 0; i < ref.size(); i++){
-                    cout << "dict: " << ref[i] << endl;
-                }
-            }
+void createMap(Map &abc){
+        char letter = 'a';
+        for(int i = 0; i < 26; i++){
+            abc[letter] = 0;
+            letter++;
         }
     }
 
+int main(){
+    string in;
+    vector<string> tmp;
+    vector<vector<string>> dic;
+    Map mapp;
+    
+    createMap(mapp);
+    while(cin >> in && in != "#"){
+        tmp.push_back(in);
+        dic.push_back(tmp);
+        tmp.clear();
+    }
+    cin.ignore();
+    while(getline(cin, in) && in != "#"){
+        int cont = 0;
+        bool flag = true;
+        for(int i = 0; i < in.size(); i += 2){
+            mapp[in[i]]++;
+        }
+        Map copy = mapp;
+        for(int i = 0; i < dic.size(); i++){
+            for(int j = 0; j < dic[i].size(); j++){
+                for(int k = 0; k < dic[i][j].size(); k++){
+                    if(mapp[dic[i][j][k]] > 0 && flag){
+                        mapp[dic[i][j][k]]--;
+                    }else{
+                        flag = false;
+                    }
+                    
+                }
+                mapp = copy;
+                if(flag){
+                    cont++;
+                }
+                flag = true;
+            }
+        }
+        cout << cont << endl;
+        cont = 0;
+        mapp.clear();
+    }
     return 0;
 }
